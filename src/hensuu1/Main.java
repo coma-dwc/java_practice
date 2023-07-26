9f03f3a22e6f2aea3e410c6362ae072e40e8ebb8
coma-dwc <tamagomapon@gmail.com>
2023-07-26 12:45:42 +0900

    first commit

---
+++ src/hensuu1/Main.java
@@ -0,0 +1,17 @@
+package hensuu1;
+//変数の定義
+class Main {
+	public static void main(String[] args) {
+		//int型の変数numberを定義
+		int number;
+		number = 3;
+		
+		System.out.println(number);
+		
+		//String型の変数nameを定義
+		String name;
+		name = "Kate";
+		
+		System.out.println(name);
+	}
+}
