90cf4d7375f2f7b54def83daa0e80e33cf6f5156
coma-dwc <tamagomapon@gmail.com>
2023-07-26 16:35:54 +0900

    変数の更新　finish

---
+++ src/hensuu_koshin/Main.java
@@ -0,0 +1,22 @@
+package hensuu_koshin;
+//変数の更新
+class Main {
+	public static void main(String[] args) {
+		int number = 11;
+		String text = "Ruby";
+		System.out.println(number);
+		System.out.println(text);
+		
+		//変数numberを9で上書き
+		number = 9;
+		
+		//変数numberを出力
+		System.out.println(number);
+		
+		//変数textを「Java」で上書き
+		text = "Java";
+		
+		//変数textを出力
+		System.out.println(text);
+	}
+}
