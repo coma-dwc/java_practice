1ad910c4881818973784baae80feb29ca54ab6d6
coma-dwc <tamagomapon@gmail.com>
2023-07-26 17:48:00 +0900

    変数 自己代入 finish

---
+++ src/hensuu_dainyu/Main.java
@@ -0,0 +1,34 @@
+package hensuu_dainyu;
+//自己代入
+//class Main {
+//	public static void main(String[] args) {
+//		int number = 3;
+//		System.out.println(number);
+//		
+//		//変数numberの値に7を足して、変数numberを上書き
+//		number = number + 7;
+//		
+//		//変数numberを出力
+//		System.out.println(number);
+//	}
+//}
+
+
+//自己代入の省略
+class Main {
+	public static void main(String[] args) {
+		int number = 8;
+		
+		//変数numberに7をかけて、変数nu,berを上書き
+		number *= 7;
+		
+		//変数numberを出力
+		System.out.println(number);
+		
+		//変数numberび値に1を足して、変数numberを上書き
+		number ++;
+		
+		//変数numberを出力
+		System.out.println(number);
+	}
+}
