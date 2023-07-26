7c6db2a276596114f1dac894b6c8fc5f15d074bb
coma-dwc <tamagomapon@gmail.com>
2023-07-26 13:22:44 +0900

    hensuu2 finish

---
+++ src/hensuu2/Main.java
@@ -0,0 +1,19 @@
+package hensuu2;
+//変数を使ってみよう
+class Main {
+	public static void main(String[] args) {
+		int number1 = 3;
+		
+		//int型の変数number2を定義し、7を代入する
+		int number2 = 7;
+		
+		//number1 * number2を出力
+		System.out.println(number1 * number2);
+		
+		//変数textに「プログラミング学習」を代入
+		String text = "プログラミング学習";
+		
+		//「今日も」と変数textを連結して出力する
+		System.out.println("今日も" + text);
+	}
+}
