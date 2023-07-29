7d87a7b6a50fd097f37db2290595942d011e9f52
coma-dwc <tamagomapon@gmail.com>
2023-07-29 22:12:11 +0900

    クラス finish

---
+++ src/class_1/Main.java
@@ -0,0 +1,7 @@
+package class_1;
+
+class Main {
+	  public static void main(String[] args) {
+	    Person.hello();
+	  }
+	}
---
+++ src/class_1/Person.java
@@ -0,0 +1,8 @@
+package class_1;
+
+class Person {
+	  public static void hello() {
+	    // "Hello World"を、"Hello Java"に書き換え
+	    System.out.println("Hello Java");
+	  }
+	}
